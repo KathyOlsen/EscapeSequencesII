8259cb97d4be9f591605aa57bb6dbc1bdccc2bac
KathyOlsen <katherinekolsen@verizon.net>
2019-09-10 10:37:30 -0400

    Initial commit

---
+++ src/ColumnTotals.java
@@ -0,0 +1,9 @@
+public class ColumnTotals {
+    public static void main(String[] args){
+        //print table of figures using tabs between columns
+        System.out.println("Cost\t\tQuantity\tTotal");
+        System.out.println("$1,000.00\t\t4\t$4,000.00\n$   50.00\t\t8\t$  400.00");
+        System.out.println("_____________________________");
+        System.out.println("TOTAL:\t\t\t12\t$4,400.00");
+    }
+}
